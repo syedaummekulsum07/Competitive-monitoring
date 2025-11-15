b0845bb4387a9f315df55dffb665945c60d3b46a
Syeda Umme Kulsum <syedaummekulsum9@gmail.com>
2025-11-15 22:28:50 +0530

    did somes change in server file

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const competitorRoutes = require("./routes/competitorRoutes");
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static("../public"));
+app.use(express.static("public"));
 
 mongoose.connect(`${process.env.MONGO_URI}`)
     .then(() => console.log("MongoDB connected"))
