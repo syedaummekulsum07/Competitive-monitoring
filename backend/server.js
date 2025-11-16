ddfe4c47dc98d4794b649ed49b1c5f100a920857
Syeda Umme Kulsum <syedaummekulsum9@gmail.com>
2025-11-16 11:26:57 +0530

    did some change for deployment purpose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config()
 const port = process.env.PORT
 
@@ -10,12 +11,19 @@ const competitorRoutes = require("./routes/competitorRoutes");
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static("public"));
+// Serve frontend files from "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/competitors", competitorRoutes);
+
+// Fallback for frontend (for all unmatched routes)
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 
 mongoose.connect(`${process.env.MONGO_URI}`)
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
-
-app.use("/api/competitors", competitorRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
