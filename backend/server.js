4999a32729f36a8cc8bee603746b7037454e389b
Syeda Umme Kulsum <syedaummekulsum9@gmail.com>
2025-11-16 12:41:36 +0530

    did some change to get th port in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 require("dotenv").config()
-const port = process.env.PORT
+const port = process.env.PORT || 5000;
 
 const competitorRoutes = require("./routes/competitorRoutes");
 
